b6632d16c473943000c96cd972cdb12b55bfbcb5
Naledi-Mokhethi <naledimokhethi28@gmail.com>
2021-11-16 05:56:27 +0200

    Configuring the data access layer.

---
+++ Work_Intergrated_Learning/Data/WilDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Work_Intergrated_Learning.Models;
+
+namespace Work_Intergrated_Learning.Data
+{
+    public class WilDbContext : DbContext
+    {
+        public WilDbContext(DbContextOptions<WilDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Jobs> Jobs { get; set; }
+    }
+}
---
+++ Work_Intergrated_Learning/Models/Jobs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Work_Intergrated_Learning.Models
+{
+    public class Jobs
+    {
+        public int Id { get; set; }
+        public String JobTitle { get; set; }
+        public String JobDescription { get; set; }
+        public String JobRoleNResponsibilities { get; set; }
+        public int Sorting { get; set; }
+
+    }
+}
